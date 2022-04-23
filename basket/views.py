3b0ced44e1df2ed421292834cfe85db14c43edd4
DeannaCarina <deannacarina@hotmail.com>
2022-04-23 10:37:46 +0000

    add: Shopping basket app, template and urls

---
+++ basket/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def view_basket(request):
+    """ A view to render the bag content """
+    return render(request, 'basket/basket.html')
