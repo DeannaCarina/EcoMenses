2c15a12545bc98409fcae378fbd487105c6d738e
DeannaCarina <deannacarina@hotmail.com>
2022-04-23 18:53:52 +0000

    add: Remaining of size logic, add product link to basket item

--- basket/views.py
+++ basket/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, redirect
 # Create your views here.
 
 def view_basket(request):
-    """ A view to render the bag content """
+    """ A view to render the basket content """
     return render(request, 'basket/basket.html')
 
 
@@ -11,13 +11,26 @@ def add_to_basket(request, item_id):
     """ Add a specified product quantity to shopping basket """
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    size = None
+
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
+
     basket = request.session.get('basket', {})
 
-
-    if item_id in list(basket.keys()):
-        basket[item_id] += quantity
+    if size:
+        if item_id in list(basket.keys()):
+            if size in basket[item_id]['items_by_size'].keys():
+                basket[item_id]['items_by_size'][size] += quantity
+            else:
+                basket[item_id]['items_by_size'][size] = quantity
+        else:
+            basket[item_id] = {'items_by_size': {size: quantity}}
     else:
-        basket[item_id] = quantity
+        if item_id in list(basket.keys()):
+            basket[item_id] += quantity
+        else:
+            basket[item_id] = quantity
 
     request.session['basket'] = basket
     return redirect(redirect_url)
