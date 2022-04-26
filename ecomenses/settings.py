28cddb3a4bb39645da5082124d6f7f17c7c53cf7
DeannaCarina <deannacarina@hotmail.com>
2022-04-26 15:34:31 +0000

    remove: Secret key and set debug

--- ecomenses/settings.py
+++ ecomenses/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t4$9#o2vd*&r^7k^3n%43gr11q556tmiw-#aw$rs4$wi!ktmn&'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['ecomenses.herokuapp.com', 'localhost']
 
